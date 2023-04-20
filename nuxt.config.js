4f5d766b4e98980f127d5531b95467b21867f871
BlkDem <m_sp@mail.ru>
2023-04-20 20:46:23 +0300

    bugfix

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,14 +2,13 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'Пятков Максим инженер-программист из Анталии',
-    description: 'Пятков Максим -  инженер-программист из Анталии. Фронтэнд, бэкэнд, фуллстэк.',
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'Пятков Максим -  инженер-программист из Анталии. Фронтэнд, бэкэнд, фуллстэк.' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
