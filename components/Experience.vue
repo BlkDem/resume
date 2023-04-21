5f4ecbd48390bc9d4d18d8736a50270debbf46da
BlkDem <m_sp@mail.ru>
2023-04-21 11:26:58 +0300

    edit portfolio

--- components/Experience.vue
+++ components/Experience.vue
@@ -6,10 +6,8 @@
       <!-- <p class="small-p">2020-05 - по настоящее время</p> -->
       <div class="p-gradient my-2"><p class="small-p">2020-05 - по настоящее время</p></div>
       <h4>Индивидуальный предприниматель</h4>
-      <p><a class="gold" href="https://umolab.ru" target="_blank">Umolab</a> - разработка микроконтроллеров -
-        автотюннинг,
-        моддинг ПК, IoT, Home Automation, программное
-        обеспечение, поддержка бизнеса.</p>
+      <p><a class="gold" href="https://umolab.ru" target="_blank">Umolab</a> - разработка электронных устройств на базе микроконтроллеров.
+        автотюннинг, моддинг ПК, IoT, Home Automation</p>
       <p class="bottom-p">Новосибирск, Россия</p>
 
       <div class="space-4"></div>
@@ -18,7 +16,7 @@
       <div class="p-gradient my-2"><p class="small-p">2006-06 - 2020-05</p></div>
 
       <h4>Руководитель департамента ИТ</h4>
-      <p>АО «СУЭК» - поддержка инфраструктуры, servicedesk</p>
+      <p><a href="https://www.suek.ru" target="_blank" class="gold">АО «СУЭК»</a> - поддержка инфраструктуры, servicedesk</p>
       <p class="bottom-p">Москва, Россия</p>
     <!-- </div> -->
 
