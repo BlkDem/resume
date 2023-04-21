2bc2a311e2552bc7d14566e5e5f16def1a3e2021
BlkDem <m_sp@mail.ru>
2023-04-21 15:50:30 +0300

    edit experience

--- components/Experience.vue
+++ components/Experience.vue
@@ -8,7 +8,7 @@
     <!-- <div class="m-4"> -->
 
       <!-- <p class="small-p">2020-05 - по настоящее время</p> -->
-      <div class="p-gradient my-2"><p class="small-p">2020-05 - по настоящее время</p></div>
+      <div class="p-gradient my-2"><p class="small-p">2020-05 - now</p></div>
       <h4>Индивидуальный предприниматель</h4>
       <p><a class="gold" href="https://umolab.ru" target="_blank">Umolab</a> - разработка электронных устройств на базе микроконтроллеров.
         Автотюннинг, моддинг ПК, IoT, Home Automation</p>
@@ -19,7 +19,7 @@
       <!-- <p class="small-p">2006-06 - 2020-05</p> -->
       <div class="p-gradient my-2"><p class="small-p">2006-06 - 2020-05</p></div>
 
-      <h4>Руководитель департамента ИТ</h4>
+      <h4>Руководитель ИТ департамента</h4>
       <p><a href="https://www.suek.ru" target="_blank" class="gold">АО «СУЭК»</a> - поддержка инфраструктуры, servicedesk</p>
       <p class="bottom-p">Москва, Россия</p>
     <!-- </div> -->
