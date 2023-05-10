3cfa57be4de5997cf13cbfca3dae8c1128a235a7
BlkDem <m_sp@mail.ru>
2023-05-10 17:31:24 +0300

    ++ experience

--- components/ExperienceDetail.vue
+++ components/ExperienceDetail.vue
@@ -8,6 +8,9 @@
       <h4>Основной стэк</h4>
       <p>PHP/Laravel (REST API), VueJS/NuxtJS, NodeJS, MySQL, HTML/CSS/JS</p>
 
+      <h4>Дополнительно</h4>
+      <p>Git, Docker, Postman, Bootstrap, Mosquitto, Memcached</p>
+
       <div class="space-4"></div>
 
       <!-- <h4>Контроллеры</h4>
@@ -16,7 +19,11 @@
       <div class="space-4"></div> -->
 
       <h4>ОС</h4>
-      <p>Linux/Windows, администрирование, поддержка, серверы, разработка сервисов</p>
+      <p>Linux/Windows, администрирование, поддержка, серверы, сервисы на базе systemd, supervisor, forever</p>
+      <p>Windows Server, Ubuntu, CentOS</p>
+
+      <h4>Виртуализация</h4>
+      <p>VPS/VDS, Hyper-V - установка, настройка, конфигурирование сервисов</p>
 
       <div class="space-4"></div>
 
