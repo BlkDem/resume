1a724813817d61d95267a4e73c43b5b56b2297e6
BlkDem <m_sp@mail.ru>
2023-06-21 23:30:31 +0300

    edit+update

--- components/Experience.vue
+++ components/Experience.vue
@@ -16,7 +16,7 @@
       <div class="p-gradient my-2"><p class="small-p">2006-06 - 2020-05</p></div>
 
       <h4>Руководитель ИТ департамента</h4>
-      <p><a href="https://www.suek.ru" target="_blank" class="gold">АО «СУЭК»</a> - поддержка инфраструктуры, servicedesk</p>
+      <p><a href="https://www.suek.ru" target="_blank" class="gold">АО «СУЭК»</a> - энергетическая корпорация - поддержка инфраструктуры, servicedesk</p>
       <p class="bottom-p">Москва, Россия</p>
 
   </BaseCard>
